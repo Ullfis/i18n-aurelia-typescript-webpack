98d85a0b2726f949085fa906031da7454fbd1d0f
Eyjolfur Vatne <eyjolfur.vatne@live.no>
2016-08-23 11:42:25 +0200

    fix(i18next): copy and change i18next typescript definitions

--- custom_typings/custom.d.ts
+++ custom_typings/custom.d.ts
@@ -1,3 +1,2 @@
-/// <reference path="../node_modules/aurelia-i18n/doc/i18next.d.ts" />
 /// <reference path="../node_modules/i18next-xhr-backend/typings/i18next-xhr-backend.d.ts" />
 /// <reference path="../node_modules/@types/requirejs/index.d.ts" />
